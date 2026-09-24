5822d2f6ca70f60d3260fd5b2735f5555ce77b4d
kitarabbit <kitarabbit@gmail.com>
2026-09-24 21:32:26 +0800

    Increase entry form spacing

--- dist/sw.js
+++ dist/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'food-rhythm-v13';
-const APP_SHELL = ['./', './index.html', './styles.css?v=13', './app.js?v=12', './manifest.webmanifest', './icons/app-icon.svg'];
+const CACHE_NAME = 'food-rhythm-v14';
+const APP_SHELL = ['./', './index.html', './styles.css?v=14', './app.js?v=12', './manifest.webmanifest', './icons/app-icon.svg'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
