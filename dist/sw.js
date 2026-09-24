70a37183be4ced99f633ad211be35ed87d65964b
kitarabbit <kitarabbit@gmail.com>
2026-09-24 21:46:34 +0800

    Stack mobile date and time fields

--- dist/sw.js
+++ dist/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'food-rhythm-v16';
-const APP_SHELL = ['./', './index.html', './styles.css?v=16', './app.js?v=12', './manifest.webmanifest', './icons/app-icon.svg'];
+const CACHE_NAME = 'food-rhythm-v17';
+const APP_SHELL = ['./', './index.html', './styles.css?v=17', './app.js?v=12', './manifest.webmanifest', './icons/app-icon.svg'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
